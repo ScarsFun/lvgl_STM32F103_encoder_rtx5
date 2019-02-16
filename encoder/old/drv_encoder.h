4f35fc48e6dd7e849da544a1c686808f6c0b766e
scarsfun <pusillus@yahoo.com>
2019-02-16 09:56:24 +0100

    old files deleted

--- encoder/old/drv_encoder.h
+++
@@ -1,5 +0,0 @@
-
-void TIM4_init(void);
-int16_t TIM4_read(void);
-uint8_t enc_pressed (void);
-
--- encoder/old/drv_encoder_msg.h
+++
@@ -1,6 +0,0 @@
-
-void TIM4_init(void);
-uint16_t TIM4_read(void);
-void enc_button_init(void);
-uint8_t enc_pressed (void);
-
